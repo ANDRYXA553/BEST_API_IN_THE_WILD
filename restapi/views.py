0ccc34214f65a4262bb6335bc9d5ac747e5653ba
pavlochikalo <pavlo.chikalo999@gmail.com>
2021-09-17 09:42:29 +0300

    search view

--- restapi/views.py
+++ restapi/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 from django.http import Http404
 from rest_framework import authentication, permissions
 from django.contrib.auth.models import User
@@ -9,6 +10,7 @@ from django.db.models import Q
 from django.http import Http404
 from .models import Product, Category
 from .serializer import ProductSerializer, CategorySerializer
+
 
 class ProductList(APIView):
     def get(self, request):
@@ -23,6 +25,7 @@ class ProductList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
@@ -31,12 +34,12 @@ class ProductDetail(APIView):
             raise Http404
 
     def get(self, request, category_slug, product_slug, format=None):
-        product = self.get_object(category_slug,product_slug)
+        product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
     def put(self, request, category_slug, product_slug, format=None):
-        product = self.get_object(category_slug,product_slug)
+        product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -47,6 +50,7 @@ class ProductDetail(APIView):
         product = self.get_object(category_slug, product_slug)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class CategoryList(APIView):
     def get_object(self, category_slug):
@@ -59,3 +63,15 @@ class CategoryList(APIView):
         category = self.get_object(category_slug)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
+
+
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+    if query:
+        products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"products": []})
